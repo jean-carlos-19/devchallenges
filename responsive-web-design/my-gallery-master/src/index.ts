a602b4b63136a3932d4d2bc51751e0a3656837d0
piguavejc <piguavejc@gmail.com>
2023-03-03 14:19:11 -0500

    subiendo el proyecto de la pagina my-gallery-master

---
+++ responsive-web-design/my-gallery-master/src/index.ts
@@ -0,0 +1,9 @@
+import "@img/devchallenges.png";
+import "@img/profilePhoto.png";
+import "@img/photo1.png";
+import "@img/photo2.png";
+import "@img/photo3.png";
+import "@img/photo4.png";
+import "@img/photo5.png";
+import "@img/photo6.png";
+import "@scss/main.scss";
