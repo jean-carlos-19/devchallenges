d32c2ba40841cff9a7f3e396692ee939709d5142
piguavejc <piguavejc@gmail.com>
2023-03-03 14:08:51 -0500

    subiendo proyecto de team-page

---
+++ responsive-web-design/team-page/src/index.ts
@@ -0,0 +1,8 @@
+import "@img/photo1.png";
+import "@img/photo2.png";
+import "@img/photo3.png";
+import "@img/photo4.png";
+import "@img/photo5.png";
+import "@img/photo6.png";
+import "@img/devchallenges.png";
+import "@scss/main.scss";
