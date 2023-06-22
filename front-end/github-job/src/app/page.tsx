6ef6884d96c2af065dbc598c62c8def5d41da7f7
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-06-22 16:16:51 -0500

    Modificado el archivo page.tsx: agregando componentes a la pagina

--- front-end/github-job/src/app/page.tsx
+++ front-end/github-job/src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
       <header className="header header-home">
         <h1>Github Jobs</h1>
       </header>
+
       <form>
 
         <div className="form form-home">
@@ -48,8 +49,35 @@ export default function Home() {
           <input type="radio" name="city" id="berlin" value={"berlin"} />
           <label htmlFor="berlin">Berlin</label>
         </div>
-
       </form>
+
+      <section className="repository">
+        <article className="card">
+          <figure className="figure">
+            <img src="https://media.istockphoto.com/id/1430294897/photo/beautiful-emotional-woman-with-bright-make-up.webp?s=170667a&w=0&k=20&c=Ttl0aIgStmzuqPO1pS2H2-k_RRZD5MrdhThXLxrlNJ8=" alt="" />
+            <figcaption>
+              <small>Kasisto</small>
+              <h4>Front-End Software Engineer</h4>
+              <span>Full time</span>
+              <div className="time">
+                <div>
+                  <span className="material-symbols-outlined color-gray-100">
+                    public
+                  </span>
+                  <p> New York</p>
+                </div>
+                <div>
+                  <span className="material-symbols-outlined color-gray-100">
+                    schedule
+                  </span>
+                  <p> 5 days ago </p>
+                </div>
+              </div>
+            </figcaption>
+          </figure>
+
+        </article>
+      </section>
     </main>
   )
 }
