fc61929a2aa864a119e727c4bd43197583381f62
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-06-22 12:57:35 -0500

    Modificado el archivo page.tsx: Añadiendo el estilo global de la pagina y ciertos componentes de la IU

--- front-end/github-job/src/app/page.tsx
+++ front-end/github-job/src/app/page.tsx
@@ -1,6 +1,28 @@
-
+import "../../atomic/main.scss"
 export default function Home() {
   return (
-    <main></main>
+    <main className="home">
+      <section>
+
+        <article>
+
+          <header>
+            <h1>Github Jobs</h1>
+          </header>
+
+          <form>
+            <label htmlFor="">
+              <span className="material-symbols-outlined color-gray-100">
+                schedule
+              </span>
+              <input type="text" placeholder="Title,companies,experti..." />
+              <input type="button" value="Search" />
+            </label>
+          </form>
+
+        </article>
+
+      </section>
+    </main>
   )
 }
