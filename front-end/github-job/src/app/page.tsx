46664cdc9ff22f6c50b138e3f7ea142c25ee060c
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-06-22 15:43:37 -0500

    modificado el archivo page.tsx: Añadiendo los componentes para la IU

--- front-end/github-job/src/app/page.tsx
+++ front-end/github-job/src/app/page.tsx
@@ -5,17 +5,51 @@ export default function Home() {
       <header className="header header-home">
         <h1>Github Jobs</h1>
       </header>
-      <section >
-        <form className="form form-home">
+      <form>
+
+        <div className="form form-home">
           <div className="search bg-white-100">
             <span className="material-symbols-outlined icon icon-search color-gray-100">
               business_center
             </span>
             <input className="field field-search" type="text" placeholder="Title,companies,experti..." />
-            <input className="button button-search bg-blue-100 color-white-100" type="button" value="Search"  />
+            <input className="button button-search bg-blue-100 color-white-100" type="button" value="Search" />
           </div>
-        </form>
-      </section>
+        </div>
+
+        <div className="check">
+          <input type="checkbox" name="" id="" />
+          <label htmlFor="">Full time</label>
+        </div>
+
+        <div className="location">
+          <label className="label label-location" htmlFor="">Location</label>
+          <div className="input input-location bg-white-100">
+            <span className="material-symbols-outlined icon icon-input color-gray-100">
+              public
+            </span>
+            <input className="field field-input" type="text" placeholder="City,state,zip code or country" />
+          </div>
+        </div>
+
+        <div>
+          <input type="radio" name="city" id="london" value={"london"} checked />
+          <label htmlFor="london">London</label>
+        </div>
+        <div>
+          <input type="radio" name="city" id="amsterdam" value={"amsterdam"} />
+          <label htmlFor="amsterdam">Amsterdam</label>
+        </div>
+        <div>
+          <input type="radio" name="city" id="new_york" value={"new_york"} />
+          <label htmlFor="new_york">New York</label>
+        </div>
+        <div>
+          <input type="radio" name="city" id="berlin" value={"berlin"} />
+          <label htmlFor="berlin">Berlin</label>
+        </div>
+
+      </form>
     </main>
   )
 }
