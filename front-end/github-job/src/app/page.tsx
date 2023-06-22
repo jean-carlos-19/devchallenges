65dd498abd62eade53ebcf7e062bf2a37a132aa1
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-06-22 14:52:07 -0500

    Modificado el archivo  page.tsx: añadiendo componentes para la IU

--- front-end/github-job/src/app/page.tsx
+++ front-end/github-job/src/app/page.tsx
@@ -2,26 +2,19 @@ import "../../atomic/main.scss"
 export default function Home() {
   return (
     <main className="home">
-      <section>
-
-        <article>
-
-          <header>
-            <h1>Github Jobs</h1>
-          </header>
-
-          <form>
-            <label htmlFor="">
-              <span className="material-symbols-outlined color-gray-100">
-                schedule
-              </span>
-              <input type="text" placeholder="Title,companies,experti..." />
-              <input type="button" value="Search" />
-            </label>
-          </form>
-
-        </article>
-
+      <header className="header header-home">
+        <h1>Github Jobs</h1>
+      </header>
+      <section >
+        <form className="form form-home">
+          <div className="search bg-white-100">
+            <span className="material-symbols-outlined icon icon-search color-gray-100">
+              business_center
+            </span>
+            <input className="field field-search" type="text" placeholder="Title,companies,experti..." />
+            <input className="button button-search bg-blue-100 color-white-100" type="button" value="Search"  />
+          </div>
+        </form>
       </section>
     </main>
   )
