0fe8ea2221f8a503bc6b5aece0df5d10b04a6af2
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-06-23 15:52:22 -0500

    Modificado el archivo nex.config.js: Agregando variables de entorno

--- front-end/github-job/next.config.js
+++ front-end/github-job/next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env:{
+        REACT_API_GITHUB:'https://api.github.com/users',
+        REACT_URL_LOCAL:'192.168.100.7'
+    }
+}
 
 module.exports = nextConfig
